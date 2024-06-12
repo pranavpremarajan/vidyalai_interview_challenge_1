de583a99baed53a485f9529ab2e25cec6a0f57e6
unknown <pranav.mullath@gmail.com>
2024-06-12 21:40:07 +0530

    Replace dummy post images in /api/v1/posts

--- server/posts/posts.router.js
+++ server/posts/posts.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { fetchPosts } = require('./posts.service');
+const { fetchPosts, fetchPostImages } = require('./posts.service');
 const { fetchUserById } = require('../users/users.service');
 
 const router = express.Router();
@@ -7,21 +7,15 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const posts = await fetchPosts();
 
-  const postsWithImages = posts.reduce((acc, post) => {
-    // TODO use this route to fetch photos for each post
-    // axios.get(`https://jsonplaceholder.typicode.com/albums/${post.id}/photos`);
-    return [
-      ...acc,
-      {
-        ...post,
-        images: [
-          { url: 'https://picsum.photos/200/300' },
-          { url: 'https://picsum.photos/200/300' },
-          { url: 'https://picsum.photos/200/300' },
-        ],
-      },
-    ];
-  }, []);
+  const postsWithImagesPromises = posts.map(async post => {
+    const postImages = await fetchPostImages({ postId: post.id });
+    return {
+      ...post,
+      images: postImages,
+    };
+  });
+
+  const postsWithImages = await Promise.all(postsWithImagesPromises);
 
   res.json(postsWithImages);
 });
